087e245109e85257b81de0eeb6d7af462ba4cc00
danielalmaquio@gmail.com <danielalmaquio@gmail.com>
2020-11-11 12:55:18 +0100

    cors

--- routes/show.js
+++ routes/show.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const File = require('../models/file');
 
-router.get('/:uuid', async (req, res) => {
+router.get('/:uuid', async(req, res) => {
     try {
         const file = await File.findOne({ uuid: req.params.uuid });
         // Link expired
-        if(!file) {
-            return res.render('download', { error: 'Link has been expired.'});
-        } 
+        if (!file) {
+            return res.render('download', { error: 'El link ha expirado' });
+        }
         return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}` });
-    } catch(err) {
-        return res.render('download', { error: 'Something went wrong.'});
+    } catch (err) {
+        return res.render('download', { error: 'Algo ha ido mal' });
     }
 });
 
